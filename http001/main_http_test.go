730a02d2ccd624db06e02c447dec1365865f4a25
MaximSunShine <103690412+MaximSunShine@users.noreply.github.com>
2022-12-25 02:18:30 +0300

    one Handler
    one handler

--- http001/main_http_test.go
+++ http001/main_http_test.go
@@ -7,79 +7,35 @@ import (
 	"testing"
 )
 
-func TestCheckStatusOK(t *testing.T) {
-	req, err := http.NewRequest("GET", "/CheckStatusOK", nil)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(CheckStatusOK)
-	handler.ServeHTTP(rr, req)
-
-	status := rr.Code
-	if status != http.StatusOK {
-		t.Errorf("handler returned %v", status)
-	}
-	expect := `Fine!`
-	if rr.Body.String() != expect {
-		t.Errorf("handler returned %v", rr.Body.String())
-	}
+type request struct {
+	url    string
+	status int
 }
-func TestInvalidMethod(t *testing.T) {
-	req, err := http.NewRequest("GET", "/NotAllowed", nil)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(InvalidMethod)
-	handler.ServeHTTP(rr, req)
 
-	status := rr.Code
-	if status != http.StatusMethodNotAllowed {
-		t.Errorf("handler returned %v", status)
-	}
-	expect := `NotAllowed!`
-	if rr.Body.String() != expect {
-		t.Errorf("handler returned %v", rr.Body.String())
-	}
-}
-func TestInternalServerError(t *testing.T) {
-	req, err := http.NewRequest("GET", "/InternalServerError", nil)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(InternalServerError)
-	handler.ServeHTTP(rr, req)
+func TestHandlerAll(t *testing.T) {
 
-	status := rr.Code
-	if status != http.StatusInternalServerError {
-		t.Errorf("handler returned %v", status)
-	}
-	expect := `InternalServerError!`
-	if rr.Body.String() != expect {
-		t.Errorf("handler returned %v", rr.Body.String())
-	}
-}
-func TestNotFound(t *testing.T) {
-	req, err := http.NewRequest("GET", "/NotFound", nil)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(NotFound)
-	handler.ServeHTTP(rr, req)
+	var r = []request{
+		{"/NotAllowed", http.StatusMethodNotAllowed},
+		{"/StatusOK", http.StatusOK},
+		{"/InternalServerError", http.StatusInternalServerError},
+		{"/NotFound", http.StatusNotFound}}
+	for _, val := range r {
+		req, err := http.NewRequest("GET", val.url, nil)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(HandlerAll)
+		handler.ServeHTTP(rr, req)
 
-	status := rr.Code
-	if status != http.StatusNotFound {
-		t.Errorf("handler returned %v", status)
-	}
-	expect := `NotFound!`
-	if rr.Body.String() != expect {
-		t.Errorf("handler returned %v", rr.Body.String())
+		status := rr.Code
+		if status != val.status {
+			t.Errorf("handler returned %v", status)
+		}
+		/*expect := `Fine!`
+		if rr.Body.String() != expect {
+			t.Errorf("handler returned %v", rr.Body.String())
+		}*/
 	}
 }
