7dc1de5a0579eaadadd9a8951d7ea4b9a7ec5d10
MaximSunShine <103690412+MaximSunShine@users.noreply.github.com>
2022-12-25 02:18:30 +0300

    one Handler
    one handler

--- http001/main_http.go
+++ http001/main_http.go
@@ -6,44 +6,36 @@ import (
 	"net/http"
 )
 
-type Handler struct{}
-
-func (h *Handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	_, err := w.Write([]byte("Свали от сюда"))
-	if err != nil {
-		log.Println(err)
+func HandlerAll(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/NotAllowed":
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, `NotAllowed!`)
+	case "/StatusOK":
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, `Fine!`)
+	case "/InternalServerError":
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, `InternalServerError!`)
+	case "/NotFound":
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, `NotFound!`)
 	}
+
+	fmt.Fprintf(w, "Path = "+r.URL.Path)
+	//fmt.Fprintf(w)
 }
-func InvalidMethod(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusMethodNotAllowed)
-	fmt.Fprintf(w, `NotAllowed!`)
-}
-func CheckStatusOK(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, `Fine!`)
-}
-func InternalServerError(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusInternalServerError)
-	fmt.Fprintf(w, `InternalServerError!`)
-}
-func NotFound(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprintf(w, `NotFound!`)
+
+func RunWebServer() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", HandlerAll)
+
+	log.Println("Запуск веб-сервера")
+	err := http.ListenAndServe(":9090", mux)
+	log.Fatal(err)
 }
 
 func main() {
-	//простой способ обработки командной строки
-	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
-		_, err := w.Write([]byte("hello"))
-		if err != nil {
-			log.Println(err)
-		}
-	})
 
-	http.HandleFunc("/NotAllowed", InvalidMethod)
-	http.HandleFunc("/StatusOK", CheckStatusOK)
-	http.HandleFunc("/InternalServerError", InternalServerError)
-	http.HandleFunc("/NotFound", NotFound)
-
-	log.Println(http.ListenAndServe(":9090", nil))
+	RunWebServer()
 }
