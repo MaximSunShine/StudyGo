4fb01c2b0ddf201d86245c476e77f7b4e215f756
MaximSunShine <103690412+MaximSunShine@users.noreply.github.com>
2023-01-23 23:33:04 +0300

    lesson 2.1

--- GS/http-rest-api/internal/app/store/userrepository_test.go
+++ GS/http-rest-api/internal/app/store/userrepository_test.go
@@ -7,29 +7,31 @@ import (
 	"testing"
 )
 
+// добавление строки в таблицу
 func TestUserRepository_Create(t *testing.T) {
-	s, teardown := store.TestStore(t, databaseURL)
-	defer teardown("users")
+	s, teardown := store.TestStore(t, databaseURL) //создаем БД и получаем структуру и фн удаление таблиц с бд
+	defer teardown("users")                        // удаление таблицы
 
-	u, err := s.User().Create(&model.User{
+	u, err := s.User().Create(&model.User{ // добавление строки в таблицу
 		Email: "user@example.org",
 	})
-	assert.NoError(t, err)
-	assert.NotNil(t, u)
+	assert.NoError(t, err) // проверка ошибки
+	assert.NotNil(t, u)    // проверка наличия заполненной структуры
 }
 
+// Поиск строки по емэйлу
 func TestUserRepository_FindByEmail(t *testing.T) {
-	s, teardown := store.TestStore(t, databaseURL)
-	defer teardown("users")
+	s, teardown := store.TestStore(t, databaseURL) //создаем БД и получаем структуру и фн удаление таблиц с бд
+	defer teardown("users")                        // удаление таблицы
 
 	email := "user@example.org"
-	u, err := s.User().FindByEmail(email)
+	u, err := s.User().FindByEmail(email) //Поиск строки по емэйлу
 	assert.Error(t, err)
 
-	u, err = s.User().Create(&model.User{
+	u, err = s.User().Create(&model.User{ //создаем струку с искомым емэйлом и проверяем ее поиск
 		Email: "user@example.org",
 	})
-	u, err = s.User().FindByEmail(email)
+	u, err = s.User().FindByEmail(email) //поиск должен завершиться удачно
 	assert.NoError(t, err)
 	assert.NotNil(t, u)
 }
---
+++ GS/http-rest-api/internal/app/test/test.go
@@ -0,0 +1,8 @@
+package test
+
+var i = 8
+
+func c() {
+	i := 8
+	i++
+}
