8aae97f1d926371a8f1c86e5015d249cf31a8ef1
MaximSunShine <103690412+MaximSunShine@users.noreply.github.com>
2023-04-15 08:37:53 +0300

    Done

---
+++ Leetcode/Reverse Integer N7/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"math"
+	"strconv"
+)
+
+const (
+	minInt32 = int64(math.MinInt32) // -1 << 31
+	maxInt32 = int64(math.MaxInt32) // 1 << 31 -1
+)
+
+func reverse(n int) int {
+
+	a := strconv.Itoa(n)
+	minus := ""
+	b := ""
+	if string(a[0]) == "-" {
+		minus = string(a[0])
+		a = a[1:]
+	}
+
+	for _, val := range a {
+		b = string(val) + b
+	}
+
+	b = minus + b
+
+	help, err := strconv.ParseInt(b, 10, 32)
+
+	if err != nil {
+		help = 0
+	}
+
+	return int(help)
+}
+
+func main() {
+	//2147483647
+	//1563847412
+	//-2147483648
+	//-1563847412
+	var n = 1563847412
+	n = reverse(n)
+	println(n)
+}
