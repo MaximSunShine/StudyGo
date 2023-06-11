49e978ee62d8ff1bc3cc11529a49ae3c8fbd7059
MaximSunShine <103690412+MaximSunShine@users.noreply.github.com>
2023-06-11 09:56:18 +0300

    Update main.go

--- Leetcode/Substring with Concatenation of All Words N30/main.go
+++ Leetcode/Substring with Concatenation of All Words N30/main.go
@@ -1,5 +1,45 @@
 package main
+import "fmt"
 
+func find(s string, a[]string) bool{
+	
+	if len(s) > 0 {
+		
+	}
+	return true
+}
+
+func is(s string, a []string) (int, bool){
+	for i, val := range a {
+		if s == val {
+			return i, true
+		}
+	}
+	return -1, false
+	
+}
+
+func f(s string, a []string) []int {
+	
+	if i, ok := is(s[:3],a); ok{
+		
+		fmt.Println("-> ", s[3:], append(a[:i], a[i+1:]...) )
+		//f(s[3:],a)
+	}
+	
+	hash := make(map[string]struct{})
+	
+	for _, val := range a {
+		hash[val] = struct{}{}
+	}
+	
+	for val := range hash {
+		fmt.Println(val)
+	}
+	
+	return []int{1}
+}
 func main() {
+	fmt.Println( f("abcabc", []string{"abc","def"}) )
 
 }
